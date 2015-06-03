cf37c1bc67067144421b171b94379cb2ae026a52
Pablo Gonzalez <pablo.gonzalez.granados@gmail.com>
2015-06-03 19:34:13 +0200

    init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name microrrelatosApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the microrrelatosApp
+ */
+angular.module('microrrelatosApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
